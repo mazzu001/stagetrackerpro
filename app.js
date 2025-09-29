3c8f024ce21a3e3846d9752a6b932a0bd29640ef
Matthew Guenther <mazzu001@hotmail.com>
2025-09-29 18:07:46 -0500

    Add fullscreen toggle option to settings menu

--- app.js
+++ app.js
@@ -694,10 +694,39 @@ async function handleImportFile(e) {
 	// Show preview card and, on confirm, run import and refresh UI
 	await showImportPreview(file, { onAfterImport: refreshUIAfterImport });
 }
+// Fullscreen functionality
+function toggleFullscreen() {
+  if (!document.fullscreenElement) {
+    // Enter fullscreen
+    if (document.documentElement.requestFullscreen) {
+      document.documentElement.requestFullscreen();
+    } else if (document.documentElement.mozRequestFullScreen) { // Firefox
+      document.documentElement.mozRequestFullScreen();
+    } else if (document.documentElement.webkitRequestFullscreen) { // Chrome, Safari & Opera
+      document.documentElement.webkitRequestFullscreen();
+    } else if (document.documentElement.msRequestFullscreen) { // IE/Edge
+      document.documentElement.msRequestFullscreen();
+    }
+  } else {
+    // Exit fullscreen
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    } else if (document.mozCancelFullScreen) { // Firefox
+      document.mozCancelFullScreen();
+    } else if (document.webkitExitFullscreen) { // Chrome, Safari & Opera
+      document.webkitExitFullscreen();
+    } else if (document.msExitFullscreen) { // IE/Edge
+      document.msExitFullscreen();
+    }
+  }
+}
+
 if (audioFileInput) audioFileInput.addEventListener('change', handleAudioFiles);
 if (exportArchiveBtn) exportArchiveBtn.addEventListener('click', () => { showExportPreview(); closeSettingsMenu(); });
 if (importArchiveBtn) importArchiveBtn.addEventListener('click', () => { importArchive(); closeSettingsMenu(); });
 if (archiveFileInput) archiveFileInput.addEventListener('change', handleImportFile);
+const fullscreenBtn = document.getElementById('fullscreenBtn');
+if (fullscreenBtn) fullscreenBtn.addEventListener('click', () => { toggleFullscreen(); closeSettingsMenu(); });
 // Reset Storage button removed from menu
 // if (resetStorageBtn) resetStorageBtn.addEventListener('click', async () => {
 //	closeSettingsMenu();
